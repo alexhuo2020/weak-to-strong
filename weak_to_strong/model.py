d938cc397c409fa23ef02af26ab5e5669fe6778f
huo <huo@huodeMacBook-Pro.local>
2024-02-16 23:03:34 -0500

    modify model.py to enable embed output

--- weak_to_strong/model.py
+++ weak_to_strong/model.py
@@ -38,7 +38,19 @@ class TransformerWithHead(PreTrainedModel):
             model if hasattr(model, "save_pretrained") else model.module
         ).gradient_checkpointing_enable()
 
-    def forward(self, input_ids: torch.LongTensor):
+    def embed(self, input_ids:torch.LongTensor):
+        device = input_ids.device
+        b, t = input_ids.size()
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(input_ids) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
+        emd = self.transformer.drop(tok_emb + pos_emb) 
+        return emd
+
+
+    def forward(self, input_ids: torch.LongTensor, embed=None):
         """
         Forward pass of the model with a linear head.
 
@@ -48,13 +60,29 @@ class TransformerWithHead(PreTrainedModel):
         Returns:
         HeadOutput: Output dataclass containing the logits.
         """
+        # input_lens = (input_ids != 0).sum(dim=-1)
+        # transformer_outputs = self.transformer(input_ids)
+        # hidden_states = torch.stack(
+        #     [transformer_outputs[0][i, input_lens[i] - 1, :] for i in range(len(input_lens))]
+        # )
+        # self.score.to(hidden_states.device)
+        # if self.linear_probe:
+        #     hidden_states = hidden_states.detach()
+        # logits = self.score(hidden_states)
+        # return logits
+        if embed is None:
+            embed = self.embed(input_ids)
+        
+        x  = embed
+
+        for block in self.transformer.h:
+            x = block(x)[0]
+        transformer_outputs = self.transformer.ln_f(x)
         input_lens = (input_ids != 0).sum(dim=-1)
-        transformer_outputs = self.transformer(input_ids)
         hidden_states = torch.stack(
-            [transformer_outputs[0][i, input_lens[i] - 1, :] for i in range(len(input_lens))]
+            [transformer_outputs[i, input_lens[i] - 1, :] for i in range(len(input_lens))]
         )
         self.score.to(hidden_states.device)
-        if self.linear_probe:
-            hidden_states = hidden_states.detach()
         logits = self.score(hidden_states)
-        return logits
+        return logits, embed
+    
